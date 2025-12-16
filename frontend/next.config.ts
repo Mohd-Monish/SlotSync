79de0d9030bc021e880915108ae0e339d3b7f604
Mohd Monish <mohdmonish1762@csmu.ac.in>
2025-12-16 23:26:33 +0530

    Allow build with errors

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* This forces Vercel to ignore small errors and just deploy the app */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
